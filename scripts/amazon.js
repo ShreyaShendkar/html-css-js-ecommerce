80a11454eece0abfe2cfc9899d3f29db476913a8
ShreyaShendkar <shreyashendkar8@gmail.com>
2025-11-01 23:26:15 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,7 +80,15 @@ document.querySelectorAll('.js-add-to-cart')
         quantity: 1
       }); 
       }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
       
-      console.log(cart);
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
     });
 });
